f26cef55d872deec4df6582656965110276b0f81
soviethead <67398241+soviethead@users.noreply.github.com>
2021-01-28 21:51:52 +0200

    updated general

--- general.py
+++ general.py
@@ -1,47 +1,55 @@
-from ftx import FtxClient
-from twitchBotClass import TwitchBot
-from general import sendOrder
-import twitchConstants
+from userClass import TwitchUser
+import time
 
-# FTX -----------------------
-API_KEY = ""
-API_SECRET = ""
-# FTX -----------------------
-client = FtxClient(api_secret=API_SECRET, api_key=API_KEY)
+orders = ["!b", "!s", "!b-sl", "!s-sl"]
+users = []
 
-# ___________________________
-# (symbol, side, price, qty)
 
-# (symbol, side, price,qty, type)
-# ___________________________
+def firstArg(message):
+    return message.split()[0]
 
-# create bot obj
 
-twitchBot = TwitchBot(server=twitchConstants.SERVER,
-                      port=twitchConstants.PORT,
-                      oauth=twitchConstants.PASS,
-                      bot=twitchConstants.BOT,
-                      channel=twitchConstants.CHANNEL,
-                      owner=twitchConstants.OWNER)
+def sendOrder(newMessage, client, newUser, twitchBot):
+    # check if user have already sent orders in the past
+    orderSent = []
+    if not any(searchUser.name == newUser for searchUser in users):
+        if firstArg(newMessage) == orders[0] or firstArg(newMessage) == orders[1]:
+            newOrder = twitchBot.getLimitOrder(newMessage)
+            print("trying to post a limit order")
+            orderSent = twitchBot.postLimit(newOrder, client)
+            print("limit has been sent")
+        elif firstArg(newMessage) == orders[2] or firstArg(newMessage) == orders[3]:
+            newOrder = twitchBot.getTriggerOrder(newMessage)
+            orderSent = twitchBot.postTrigger(newOrder, client)
+            print("trigger has been sent")
+        userOrderTime = time.time()
+        userPosition = newMessage.split()[0]
+        users.append(TwitchUser(newUser, userPosition, userOrderTime, client))
+        for user in users:
+            if user.name == newUser:
+                user.initTimeout(userPosition, userOrderTime)
+                user.rememberOrder(orderSent)
+                #print(user.getOrders())
 
-twitchBot.joinChat()
+    else:
+        for user in users:
+            print("checking time out")
+            if user.name == newUser and user.checkTimeout(firstArg(newMessage)):
+                print("Cheking what order")
+                if firstArg(newMessage) == orders[0] or firstArg(newMessage) == orders[1]:
+                    newOrder = twitchBot.getLimitOrder(newMessage)
+                    orderSent = twitchBot.postLimit(newOrder, client)
+                elif firstArg(newMessage) == orders[2] or firstArg(newMessage) == orders[3]:
+                    print("this is a trigger order")
+                    newOrder = twitchBot.getTriggerOrder(newMessage)
+                    print("generated trigger order")
+                    orderSent = twitchBot.postTrigger(newOrder, client)
+                    print("trigger has been sent")
+                # newOrder = twitchBot.getLimitOrder(newMessage)
+                # twitchBot.postLimit(newOrder, client)
+                orderTime = time.time()
+                user.initTimeout(firstArg(newMessage), orderTime)
+                user.rememberOrder(orderSent)
+                #user.updateOpenOrders(client, twitchBot.ticker)
+                #print(user.getOrders())
 
-# allOrders = []
-
-# start reading chat
-while True:
-    try:
-        readbuffer = twitchBot.irc.recv(1024).decode()
-    except:
-        readbuffer = ""
-    for text in readbuffer.split("\r\n"):
-        if text == "":
-            continue
-        else:
-            # get user name, get message text
-            newUser = twitchBot.getUser(text)
-            newMessage = twitchBot.getMessage(text)
-            if newMessage[0] == "!":
-                if twitchBot.isOrder(newMessage):   # and twitchBot.whatOrder(newMessage)
-                    print("trying")
-                    sendOrder(newMessage, client, newUser, twitchBot)
